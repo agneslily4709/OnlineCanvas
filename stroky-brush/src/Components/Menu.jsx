2dcaccb127bb91ca1d17366350dbd6b32798a4be
AGNES LILY <142089703+agneslily4709@users.noreply.github.com>
2023-10-11 22:46:40 +0530

    Update Menu.jsx

--- stroky-brush/src/Components/Menu.jsx
+++ stroky-brush/src/Components/Menu.jsx
@@ -6,16 +6,17 @@ const Menu = ({ setLineColor, setLineWidth,
 	return ( 
 		<div className="Menu"> 
                 <div className="menu-item">
+
                 <label>Brush Color </label> 
-			<input type="color"onChange={(e) => { setLineColor(e.target.value); }} /> 
+			<input type="color"onChange={(e) => {setLineColor(e.target.value); }} /> 
                 </div>
                 <div className="menu-item">
                         <label>Brush Width </label> 
-			<input type="range"min="3"max="20"onChange={(e) => { 	setLineWidth(e.target.value); }} /> 
+			<input type="range"min="3"max="20"onChange={(e) => {setLineWidth(e.target.value); }} /> 
                 </div>
                 <div className="menu-item">
                         <label>Brush Opacity</label> 
-			<input type="range"min="0"max="1"  step={0.1}defaultValue={1}onChange={(e) => { 	setLineOpacity(e.target.value); }} />
+			<input type="range"min="0"max="1"  step={0.1}defaultValue={1}onChange={(e) => {setLineOpacity(e.target.value); }} />
                 </div>
 		</div> 
 	); 
